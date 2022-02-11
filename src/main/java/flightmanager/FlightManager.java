d5137f232b2a4f5f95e4a7674b1d69180a198800
Amna <amna.bashir2@gmail.com>
2022-02-11 21:59:38 +0000

    half tests done for flight manager class

--- src/main/java/flightmanager/FlightManager.java
+++ src/main/java/flightmanager/FlightManager.java
@@ -7,13 +7,36 @@ import java.util.ArrayList;
 
 public class FlightManager {
     private Plane plane;
-    private ArrayList<Passenger>passenger;
+    private ArrayList<Passenger>passengers;
 
     public FlightManager(Plane plane){
         this.plane = plane;
-        this.passenger =new ArrayList<>();
+        this.passengers =new ArrayList<>();
     }
+
+    public int passengerCount(){
+       return this.passengers.size();
+    }
+
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
     public int getBaggageReservedForPassengers(){
         return (plane.getTotalWeight()/2);
     }
+//    calculate each passenger baggage weight
+    public int getEachPassengerBaggageWeight(){
+        return getBaggageReservedForPassengers() / plane.getPlaneCapacity();
+    }
+
+//    calculate how much baggage weight is booked by passengers of a flight
+    public int getTotalReservedBaggage(){
+        return getEachPassengerBaggageWeight() * passengerCount();
+    }
+
+//    calculate how much overall weight reserved for baggage remains for a flight
+    
+
+
 }
--- src/test/java/flightmanager/FlightManagerTest.java
+++ src/test/java/flightmanager/FlightManagerTest.java
@@ -2,20 +2,53 @@ package flightmanager;
 
 import org.junit.Before;
 import org.junit.Test;
+import person.Passenger;
 import person.Plane;
 
 import static org.junit.Assert.assertEquals;
 
 public class FlightManagerTest {
     FlightManager flightManager;
+    Passenger passenger;
+    Passenger passenger1;
+    Passenger passenger2;
+    Passenger passenger3;
+    Passenger passenger4;
+    Passenger passenger5;
 
     @Before
     public void before(){
        flightManager = new FlightManager(Plane.JETS);
+       passenger = new Passenger("Ruby", 4);
+       flightManager.addPassenger(passenger);
+       flightManager.addPassenger(passenger1);
+       flightManager.addPassenger(passenger2);
+       flightManager.addPassenger(passenger3);
+       flightManager.addPassenger(passenger4);
+       flightManager.addPassenger(passenger5);
+    }
+
+    @Test
+    public void getPassengerCount(){
+        assertEquals(6, flightManager.passengerCount());
+    }
+    @Test
+    public void canAddPassengers(){
+        assertEquals(6, flightManager.passengerCount());
     }
 
     @Test
     public void getBaggageWeightReserverd(){
         assertEquals(28000, flightManager.getBaggageReservedForPassengers());
+    }
+
+    @Test
+    public void calculateBagReservedForEachPassenger(){
+        assertEquals(93, flightManager.getEachPassengerBaggageWeight());
+    }
+
+    @Test
+    public void getTotalReservedBaggage(){
+        assertEquals(558, flightManager.getTotalReservedBaggage());
     }
 }
