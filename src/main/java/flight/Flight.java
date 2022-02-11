6b8afdd5cb7e7b10d76f9595d9e2b1d2b7c98dd2
Amna <amna.bashir2@gmail.com>
2022-02-11 15:28:23 +0000

    seeting flight constructor

--- src/main/java/flight/Flight.java
+++ src/main/java/flight/Flight.java
@@ -1,4 +1,29 @@
 package flight;
 
+import person.CabinCrewMember;
+import person.Passenger;
+import person.Plane;
+import pilot.Pilot;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.util.ArrayList;
+
 public class Flight {
+    private Pilot pilot;
+    private ArrayList<CabinCrewMember>cabinCrewMembers;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String departureAirport;
+    private Time departureTime;
+
+    public Flight(Pilot pilot, ArrayList<CabinCrewMember> cabinCrewMembers,Plane plane, String flightNumber, String
+            destination, String departureAirport, Time departureTime){
+        this.pilot = pilot;
+        this.cabinCrewMembers = new ArrayList<CabinCrewMember>();
+        this.passengers = new ArrayList<>();
+        
+    }
 }
