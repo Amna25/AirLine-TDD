5c9233f4a7973d470346431b431c83e0c6a9915e
Amna <amna.bashir2@gmail.com>
2022-02-11 14:59:32 +0000

    create enum plane class

---
+++ src/main/java/person/Plane.java
@@ -0,0 +1,23 @@
+package person;
+
+public enum Plane {
+    JETS(300, 56000),
+    PISTONAIRCRAF(600, 70000),
+    AIRBUS(544, 50000);
+
+    private final int planeCapacity;
+    private final int totalWeight;
+
+   Plane(int planeCapacity,int totalWeight){
+       this.planeCapacity = planeCapacity;
+       this.totalWeight = totalWeight;
+   }
+
+   public int getPlaneCapacity(){
+       return planeCapacity;
+   }
+
+   public int getTotalWeight(){
+       return totalWeight;
+   }
+}
