cf03ddf8103960d29c90561300218159914b0e8c
Amna <amna.bashir2@gmail.com>
2022-02-11 12:52:45 +0000

    pass first two test in pilot test

--- src/test/java/pilot/PilotTest.java
+++ src/test/java/pilot/PilotTest.java
@@ -1,7 +1,10 @@
 package pilot;
 
 import org.junit.Before;
+import org.junit.Test;
 import person.Rank;
+
+import static org.junit.Assert.assertEquals;
 
 public class PilotTest {
     Pilot pilot;
@@ -9,5 +12,14 @@ public class PilotTest {
     public void before(){
         pilot = new Pilot("Bilal", Rank.CAPTION, "KER34");
     }
-    
+    @Test
+    public void canHaveName(){
+        assertEquals("Bilal", pilot.getName());
+    }
+    @Test
+    public void canHaveRank(){
+        assertEquals(Rank.CAPTION, pilot.getRank());
+    }
+
+
 }
